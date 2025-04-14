3dc2ae12627a7dc063af704c62817659b4e9847e
zafarali56 <zafaralyjamali722@gmail.com>
2025-04-14 12:44:57 +0500

    for complete with stride function

--- playGround/swiftPlayGround.playground/Pages/controlflow_forLoop.xcplaygroundpage/Contents.swift
+++ playGround/swiftPlayGround.playground/Pages/controlflow_forLoop.xcplaygroundpage/Contents.swift
@@ -17,4 +17,17 @@ for index in 1...range {
     print(index)
 }
 
+var dict : Dictionary<String,String> = ["YUL":"montral", "kr":"karachi", "om": "oma"]
+for (key, types) in dict{
+    print("key",key, "value", types)
+}
+for i in stride(from: 0, to: 100, by: 5){
+    print("i \(i)")
+}
+
+for i in stride(from: 0, through: 100, by: 5){
+    print("i \(i)")
+}
+
+
 
